132e325d6811aea4baf1a08a75bc96313a319f30
Rui Carneiro <rui.motacarneiro@heig-vd.ch>
2022-12-22 14:32:44 +0100

    Removed set position function

--- chess-project/src/chess/engine/Position.java
+++ chess-project/src/chess/engine/Position.java
@@ -4,14 +4,6 @@ public class Position {
     int x;
     int y;
 
-    public void setX(int x) {
-        this.x = x;
-    }
-
-    public void setY(int y) {
-        this.y = y;
-    }
-
     public int getX() {
         return x;
     }
