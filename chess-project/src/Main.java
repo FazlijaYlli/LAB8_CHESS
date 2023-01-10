9938ff5c623503e9b7256ecd5cab7d2195eded4c
Kylian <42773865+kylianmanzini@users.noreply.github.com>
2023-01-10 15:21:22 +0100

    final

--- chess-project/src/Main.java
+++ chess-project/src/Main.java
@@ -1,12 +1,14 @@
 import chess.ChessController;
 import chess.Controller;
 import chess.ChessView;
+import chess.views.console.ConsoleView;
 import chess.views.gui.GUIView;
 
 public class Main {
     public static void main(String[] args) {
         ChessController c = new Controller();
         ChessView v = new GUIView(c);
+        //ChessView v = new ConsoleView(c);
         c.start(v);
     }
 }
